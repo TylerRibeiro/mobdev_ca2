46c79a137922c0fa5f2a772894eaedee38d71580
TylerRibeiro <ribeirotay92@gmail.com>
2023-07-06 22:35:16 +0100

    hotfix for weather services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { NewsComponent } from "./news/news.component";
+import { WeatherComponent } from "./weather/weather.component";
+import { AboutComponent } from "./about/about.component";
 
 const routes: Routes = [
+  { path: "news", component: NewsComponent },
+  { path: "weather", component: WeatherComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { WeatherService } from "../services/weather.service";
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss'],
+  selector: "app-weather",
+  templateUrl: "./weather.component.html",
+  styleUrls: ["./weather.component.scss"],
 })
-export class WeatherComponent  implements OnInit {
+export class WeatherComponent implements OnInit {
+  weatherData: any;
+  city: string = "London"; // Default city is London
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getWeather();
+  }
 
+  getWeather() {
+    this.weatherService.getWeather(this.city).subscribe((data: any) => {
+      this.weatherData = data;
+    });
+  }
+
+  search(city: string) {
+    this.city = city;
+    this.getWeather();
+  }
 }
