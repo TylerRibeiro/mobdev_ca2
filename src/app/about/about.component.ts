a2fa7697f6dc975a6295c0936d716383121af8d5
TylerRibeiro <ribeirotay92@gmail.com>
2023-07-07 23:17:21 +0100

    finished last touches and about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
-export class AboutComponent  implements OnInit {
+export class AboutComponent implements OnInit {
+  teamMembers = [
+    {
+      name: "Member 1",
+      role: "Role 1",
+      avatar: "path/to/avatar1.png",
+      photo: "path/to/photo1.png", // Add this
+      bio: "Bio for Member 1", // And this
+    },
+    {
+      name: "Member 2",
+      role: "Role 2",
+      avatar: "path/to/avatar2.png",
+      photo: "path/to/photo2.png", // Add this
+      bio: "Bio for Member 2", // And this
+    },
+    // more members...
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms"; // import FormsModule
 import { WeatherComponent } from "./weather/weather.component";
 import { NewsComponent } from "./news/news.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
-  declarations: [AppComponent, WeatherComponent, NewsComponent],
+  declarations: [AppComponent, WeatherComponent, NewsComponent, AboutComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
